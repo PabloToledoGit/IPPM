2ea0b12107493adbb52b1247b6f0a3e95cdf044e
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-08 18:48:59 -0300

    atualizando titulo para neon

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+
+function footer() {
+
+
+}
+
+
+export default footer;
--- src/components/Titulo/index.js
+++ src/components/Titulo/index.js
@@ -2,8 +2,8 @@ import "./styles.css";
 
 function Titulo() {
   return (
-    <div className="d-flex align-itens-center justify-content-center p-3 mt-4 bg-dark">
-      <h1 className="titulo">
+    <div className="d-flex align-itens-center justify-content-center p-3 mt-4">
+      <h1 className="titulo neon-header">
         Live <strong className="IPPM">I<strong className="PP">PP</strong>M</strong>
       </h1>
     </div>
