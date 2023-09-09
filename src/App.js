7d5f8e1a4e0dc95c6b6f214dda6dfcc83cd8a02f
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-09-09 16:13:35 -0300

    ajustando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LiveVideo from './components/Live';
 import Titulo from './components/Titulo';
+import footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Titulo />
       <LiveVideo />
+      <footer />
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,13 @@ import './styles.css';
 
 
 function footer() {
-
+    return(
+        <section>
+            <h1>A live pode acontecer tambem em:</h1>
+            <a href="" target="blank">Youtube</a>
+            <a href="" target="blank">Instagram</a>
+        </section>
+    )
 
 }
 
